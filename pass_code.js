79aa9627fe0d769938feff9a5723fc56e7ccaa38
maine-bit <93411663+maine-bit@users.noreply.github.com>
2024-11-11 06:23:25 +0800

    Add files via upload

---
+++ pass_code.js
@@ -0,0 +1,12 @@
+// JavaScript to check the password
+function checkPassword() {
+    var password = document.getElementById("passwordInput").value;
+    var correctPassword = "goat"; // Set the correct password here
+    
+    if (password === correctPassword) {
+      document.getElementById("passwordOverlay").style.display = "none";
+      document.getElementById("mainContent").style.display = "block";
+    } else {
+      document.getElementById("errorMessage").style.display = "block";
+    }
+  }
